01b64ac17a0a72093bd9593cbc3a97fd2f7ff079
Yangp <yangpeng@visionet.com.cn>
2017-11-15 14:08:53 +0800

    单元测试

---
+++ src/test/java/startTest/StartTest.java
@@ -0,0 +1,21 @@
+package startTest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Created by yangp on 2017/11/14.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StartTest {
+
+
+    @Test
+    public void contextloads(){
+
+    }
+
+}
