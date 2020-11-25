0d54e8b23c9fac046be9dade615ba0d89cba6593
peng.yang <peng.yang@baozun.com>
2020-11-25 13:59:06 +0800

    测试

--- src/main/java/com/example/springboot2/yang/utils/mail/MailTest.java
+++ src/main/java/com/example/springboot2/yang/utils/mail/MailTest.java
@@ -4,9 +4,9 @@ package com.example.springboot2.yang.utils.mail;
  * 测试
  */
 public class MailTest {
-    public static void main(String[] args) throws Exception {
-        MailSender mailSender = MailSender.getInstance();
-        MailInfo mailInfo = mailSender.getMailInfo();
-        mailSender.sendHtmlMail(mailInfo, 3);
+    public static void main(String[] args) {
+        int index = 3;
+        index = index + 2;
+        System.out.println(index);
     }
 }
